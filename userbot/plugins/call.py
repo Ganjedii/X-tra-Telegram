d6666c454d227af2d2b6598382223af7e7ffa453
Ganjedii <58940977+Ganjedii@users.noreply.github.com>
2020-01-03 15:30:43 +0530

    Update call.py

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -41,7 +41,7 @@ async def _(event):
             "`Connecting To Telegram Headquarters...`",
             "`Call Connected.`",
             "`Telegram: Hello This is Telegram HQ. Who is this?`",
-            "`Me: Yo this is` `Rulexx,Please Connect me to my lil bro Ganjedii`",
+            "`Me: Yo this is` `Rulexx, Please Connect me to my lil bro Ganjedii`",
             "`User Authorised.`",
             "`Calling Ganjedii`  `At +916969696969`",
             "`Private  Call Connected...`",
