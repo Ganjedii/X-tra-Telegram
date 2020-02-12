9a1af0992efecc12cd82b967cbcef034e35bbd8a
Ganjedii <58940977+Ganjedii@users.noreply.github.com>
2020-02-12 20:50:59 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -10,7 +10,7 @@ PREV_REPLY_MESSAGE = {}
 
 
 USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me.Else gaand mara dunga bsdk "
-USER_BOT_NO_WARN = "`Hello, this is Rulexx Security Service. If you spam you will be reported and blocked`"
+USER_BOT_NO_WARN = "Bleep blop! This is a bot. Don't fret.\n\n" "My master hasn't approved you to PM." "Please wait for my master to look in, he mostly approves PMs.\n\n" "As far as I know, he doesn't usually approve retards though.")# =================================================================
 
 if Var.PRIVATE_GROUP_ID is not None:
     @command(pattern="^.approve ?(.*)")
